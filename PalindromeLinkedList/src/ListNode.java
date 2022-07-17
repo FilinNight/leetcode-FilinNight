5b120c6e00311bb5fab4f21038bc1da592ca7ddc
Arthur <artur.gotovchits@mail.ru>
2022-07-17 20:20:06 +0300

    234. Palindrome Linked List.

--- PalindromeLinkedList/src/ListNode.java
+++ PalindromeLinkedList/src/ListNode.java
@@ -1,20 +1,7 @@
 public class ListNode {
-    private int val;
-
+    int val;
     ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-    public void setVal(int value) {
-        if (value > 9 || value < 0) {
-            this.val = 0;
-        } else {
-            this.val = value;
-        }
-    }
-
-    public int getVal() {
-        return this.val;
-    }
 }
